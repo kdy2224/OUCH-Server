9ccf2235d0a01b66112e518af8d88f70de9c93e1
99hyuk <99hyuk@github.com>
2024-10-18 13:45:03 +0900

    스프링 프로젝트 초기 세팅

---
+++ src/main/java/com/hy/ouch/OuchApplication.java
@@ -0,0 +1,13 @@
+package com.hy.ouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuchApplication.class, args);
+	}
+
+}
